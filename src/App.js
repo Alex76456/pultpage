0275390c7109012131500381fa7c8105d093aea6
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-03-31 19:35:06 +0300

    feat:сделать footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Carousel from './components/Carousel/Carousel';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<div className="page__container">
 				<Header />
 				<Carousel />
+				<Footer />
 			</div>
 		</div>
 	);
