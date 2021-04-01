bb4769394862e5c7b9af97cd06236bc6b203a4d3
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-04-01 09:30:53 +0300

    fix: исправить класснаймес в футере

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,101 +10,101 @@ function Footer() {
 				<div className="footer__blocks">
 					<div className="footer__block">
 						<div className="footer__logo">
-							<a href="https://www.pult.ru/" class="footer__link" target="blank">
+							<a href="https://www.pult.ru/" className="footer__link" target="blank">
 								<img className="footer__pult-icon" src={pult} alt="pult" />
 							</a>
 						</div>
 
 						<div className="footer__call">
-							<a class="footer__tel" href="tel:+78005555152" target="_blank">
+							<a className="footer__tel" href="tel:+78005555152" target="_blank">
 								8(800)555-51-52
 							</a>
-							<p class="footer__call-text">Звонок по России бесплатный</p>
+							<p className="footer__call-text">Звонок по России бесплатный</p>
 						</div>
 					</div>
-					<ul class="footer__columns">
-						<li class="footer__column">
-							<ul class="footer__list">
-								<li class="footer__list-item">
-									<a class="footer__link link" href="#">
+					<ul className="footer__columns">
+						<li className="footer__column">
+							<ul className="footer__list">
+								<li className="footer__list-item">
+									<a className="footer__link link" href="#">
 										Акустика
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Hi-Fi
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Винил
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Наушники
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Телевизоры
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Проекторы
 									</a>
 								</li>
 							</ul>
 						</li>
-						<li class="footer__column">
-							<ul class="footer__list">
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+						<li className="footer__column">
+							<ul className="footer__list">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Распродажа
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Outlet
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Акции
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Бонусы и скидки
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Рассрочка
 									</a>
 								</li>
 							</ul>
 						</li>
-						<li class="footer__column">
-							<ul class="footer__list">
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+						<li className="footer__column">
+							<ul className="footer__list">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										О компании 🡥
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Контакты 🡥
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Салоны 🡥
 									</a>
 								</li>
-								<li class="footer__list-item">
-									<a class="footer__link" href="#">
+								<li className="footer__list-item">
+									<a className="footer__link" href="#">
 										Новости 🡥
 									</a>
 								</li>
